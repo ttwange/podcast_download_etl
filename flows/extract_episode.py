5f9dba7736879493aa21896d1a3877f537f12116
ttwange <ttwange2@gmail.com>
2023-09-20 10:00:26 +0300

    Working on the extract function

--- flows/extract_episode.py
+++ flows/extract_episode.py
@@ -1,38 +1,20 @@
-import feedparser
 import requests
-from prefect import Flow, task
-
+import xmltodict
+from prefect import flow, task
 
 @task
 def fetch_podcast_episodes(feed_url):
-    # Parse the RSS feed
-    feed = feedparser.parse(feed_url)
-
-    # Extract episode information
-    episodes = []
-    for entry in feed.entries:
-        episode_title = entry.title
-        episode_url = entry.enclosures[0].href  # Assuming the podcast link is in the enclosures
-        episodes.append((episode_title, episode_url))
-
+    feed = requests.get(feed_url)
+    data = xmltodict.parse(feed.text)
+    episodes = data["rss"]["channel"]["item"]
+    print(f"Found {len(episodes)} episodes.")
     return episodes
-
-@task
-def download_episode(episode_info, download_dir):
-    episode_title, episode_url = episode_info
-    response = requests.get(episode_url)
-    if response.status_code == 200:
-        with open(f"{download_dir}/{episode_title}.mp3", "wb") as file:
-            file.write(response.content)
 
 @flow()
 def get_episodes() -> None:
     RSS_FEED_URL = "https://www.marketplace.org/feed/podcast/marketplace/"
-
     DOWNLOAD_DIR = "./episodes"
     episodes = fetch_podcast_episodes(RSS_FEED_URL)
-    download_episode.map(episodes, DOWNLOAD_DIR)
 
-# Run the Prefect flow
 if __name__ == "__main__":
-    flow.run()
+    get_episodes()
